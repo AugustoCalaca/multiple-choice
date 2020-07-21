1041ed4d7c12eec4295591ac05c586b6c1d8ab0b
AugustoCalaca <augustocalaca2@gmail.com>
2020-07-21 15:04:40 -0300

    feat(relay): graphql tag to edit a multiple choice and an updater

---
+++ packages/next/components/MultipleChoiceEdit.tsx
@@ -0,0 +1,33 @@
+import { graphql } from 'react-relay';
+import { ROOT_ID, SelectorStoreUpdater } from 'relay-runtime';
+import { connectionUpdater } from '@multiple-choice/relay';
+
+export const MultipleChoiceEdit = graphql`
+  mutation MultipleChoiceEditMutation($input: MultipleChoiceEditInput!) {
+    MultipleChoiceEdit(input: $input) {
+      error
+      multipleChoice {
+        id
+        question
+        statementA
+        statementB
+        statementC
+        statementD
+        statementE
+        correctAnswer
+      }
+    }
+  }
+`;
+
+export const updater: SelectorStoreUpdater = (store) => {
+  const newEdge = store.getRootField('MultipleChoiceEdit')?.getLinkedRecord('multipleChoice');
+
+  connectionUpdater({
+    store,
+    parentId: ROOT_ID,
+    connectionName: 'MultipleChoiceListPagination_multipleChoices',
+    edge: newEdge,
+    before: true,
+  });
+};
