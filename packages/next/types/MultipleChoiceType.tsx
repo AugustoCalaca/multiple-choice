e2d1f5373b04be134c7fc53f8fbc7a9a892cabf2
AugustoCalaca <augustocalaca2@gmail.com>
2020-07-21 15:10:28 -0300

    feat(next): anote type of the item

---
+++ packages/next/types/MultipleChoiceType.tsx
@@ -0,0 +1,11 @@
+export type MultipleChoiceType = {
+  id?: string;
+  question: string;
+  statementA: string;
+  statementB: string;
+  statementC: string;
+  statementD: string;
+  statementE: string;
+  correctAnswer?: string;
+  markedAnswer?: string;
+};
