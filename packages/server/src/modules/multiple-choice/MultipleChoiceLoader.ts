939d97bedb8d4527fec61d988caafb6c749e1ca5
AugustoCalaca <augustocalaca2@gmail.com>
2020-07-21 15:19:14 -0300

    feat(server): add multiple choice loader

---
+++ packages/server/src/modules/multiple-choice/MultipleChoiceLoader.ts
@@ -0,0 +1,13 @@
+import { createLoader, registerDataLoader } from '@multiple-choice/graphql';
+
+import MultipleChoiceModel, { IMultipleChoice } from './MultipleChoiceModel';
+
+const { Wrapper: MultipleChoice, clearCache, getLoader, load, loadAll } = createLoader<IMultipleChoice>({
+  loaderName: 'MultipleChoiceLoader',
+  model: MultipleChoiceModel,
+});
+
+registerDataLoader('MultipleChoiceLoader', getLoader);
+
+export { clearCache, getLoader, load, loadAll };
+export default MultipleChoice;
