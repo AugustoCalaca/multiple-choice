b93b4746bf2202710d1d853907d4f75d5d5927e3
AugustoCalaca <augustocalaca2@gmail.com>
2022-02-24 23:20:09 -0300

    feat: use swc jest instead babel jest

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
   rootDir: './',
   reporters: ['default'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)?$': require.resolve('babel-jest'),
+    '^.+\\.(js|jsx|ts|tsx)?$': ['@swc/jest'],
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|jsx|ts|tsx)?$',
   moduleFileExtensions: ['ts', 'js', 'tsx'],
